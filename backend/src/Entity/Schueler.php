3158e52230a02fff0f122c8f715d472cfc07cea4
root <root@LeonLaptop.localdomain>
2025-10-31 20:53:09 +0100

    schuler entity table name gegeben grosses S

--- backend/src/Entity/Schueler.php
+++ backend/src/Entity/Schueler.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: SchuelerRepository::class)]
+#[ORM\Table(name: "Schueler")]
 class Schueler
 {
     #[ORM\Id]
@@ -26,24 +27,24 @@ class Schueler
     #[ORM\Column(type: "date", nullable: true)]
     private ?\DateTimeInterface $geburtsdatum = null;
 
-    #[ORM\ManyToOne(targetEntity: Klassen::class, inversedBy: 'schueler')]
+    #[ORM\ManyToOne(targetEntity: Klassen::class, inversedBy: 'Schueler')]
     #[ORM\JoinColumn(name: "klasse_id", referencedColumnName: "id", nullable: true)]
     private ?Klassen $klasse = null;
 
-    #[ORM\OneToOne(mappedBy: 'schueler', targetEntity: Einstellungen::class)]
+    #[ORM\OneToOne(mappedBy: 'Schueler', targetEntity: Einstellungen::class)]
     private ?Einstellungen $einstellungen = null;
 
 
     #[ORM\OneToMany(mappedBy: 'zielSchueler', targetEntity: Nachrichten::class)]
     private Collection $empfangeneNachrichten;
 
-    #[ORM\OneToMany(mappedBy: 'schueler', targetEntity: NachrichtenStatus::class)]
+    #[ORM\OneToMany(mappedBy: 'Schueler', targetEntity: NachrichtenStatus::class)]
     private Collection $nachrichtenStatus;
 
-    #[ORM\OneToMany(mappedBy: 'schueler', targetEntity: KursSchueler::class)]
+    #[ORM\OneToMany(mappedBy: 'Schueler', targetEntity: KursSchueler::class)]
     private Collection $kursSchueler;
 
-    #[ORM\OneToMany(mappedBy: 'schueler', targetEntity: KursEinstellungen::class)]
+    #[ORM\OneToMany(mappedBy: 'Schueler', targetEntity: KursEinstellungen::class)]
     private Collection $kursEinstellungen;
 
     public function __construct()
