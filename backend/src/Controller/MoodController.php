d70034b3c96a86e12517ff7d5572952c71a950fb
Leon Ehart <1034@htl.rennweg.at>
2025-12-16 14:47:33 +0100

    backend controller

--- backend/src/Controller/MoodController.php
+++ backend/src/Controller/MoodController.php
@@ -1,15 +1,3 @@
-<?php
-
-namespace App\Controller;   // ✅ WICHTIG: Api entfernen
-
-use App\Entity\Schueler;
-use App\Entity\SchuelerMood;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-
 #[Route('/api/mood')]
 class MoodController extends AbstractController
 {
@@ -18,26 +6,25 @@ class MoodController extends AbstractController
         Request $request,
         EntityManagerInterface $em
     ): JsonResponse {
+        $user = $this->getUser();
+
+        if (!$user instanceof Schueler) {
+            return $this->json(['error' => 'Nicht authentifiziert'], 401);
+        }
+
         $data = json_decode($request->getContent(), true);
 
-        if (!isset($data['schueler_id'], $data['mood'])) {
-            return $this->json(['error' => 'Ungültige Daten'], 400);
+        if (!isset($data['mood'])) {
+            return $this->json(['error' => 'Mood fehlt'], 400);
         }
 
         if (!in_array($data['mood'], ['gut', 'neutral', 'schlecht'])) {
             return $this->json(['error' => 'Ungültiger Mood-Wert'], 400);
         }
 
-        $schueler = $em->getRepository(Schueler::class)
-            ->find($data['schueler_id']);
-
-        if (!$schueler) {
-            return $this->json(['error' => 'Schüler nicht gefunden'], 404);
-        }
-
         $mood = new SchuelerMood();
         $mood->setMood($data['mood']);
-        $mood->setSchueler($schueler);
+        $mood->setSchueler($user);
 
         $em->persist($mood);
         $em->flush();
@@ -46,23 +33,5 @@ class MoodController extends AbstractController
             'status' => 'ok',
             'message' => 'Mood gespeichert'
         ], 201);
-    }
-
-    #[Route('/{schuelerId}', name: 'api_mood_list', methods: ['GET'])]
-    public function list(
-        int $schuelerId,
-        EntityManagerInterface $em
-    ): JsonResponse {
-        $conn = $em->getConnection();
-
-        $data = $conn->fetchAllAssociative(
-            'SELECT erstellt_am, mood
-             FROM Schueler_Mood
-             WHERE schueler_id = ?
-             ORDER BY erstellt_am ASC',
-            [$schuelerId]
-        );
-
-        return $this->json($data);
     }
 }
