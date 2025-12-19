c4915b1a3f9fcfe30c7df0afa7ad414b1dd0ab52
Leon Ehart <1034@htl.rennweg.at>
2025-12-19 08:00:50 +0100

    Revert "lehrer routes for schueler"
    This reverts commit 5ae22704641228073718375567a4b93b36762314.

--- frontend/src/views/LogoutView.vue
+++ frontend/src/views/LogoutView.vue
@@ -1,14 +1,11 @@
 <script setup>
+// JWT entfernen
 localStorage.removeItem("token");
 document.cookie = "auth_token=; Path=/; Max-Age=0; SameSite=None; Secure";
 
-const redirectUri = encodeURIComponent(
-  "https://transparent-grading-flax.vercel.app/login"
-);
-
-window.location.href =
-  `https://login.microsoftonline.com/common/oauth2/v2.0/logout` +
-  `?post_logout_redirect_uri=${redirectUri}`;
+// Automatischer Microsoft Logout + Redirect zur Login-Seite
+const redirectUri = encodeURIComponent("https://transparent-grading-flax.vercel.app/login");
+window.location.href = `https://login.microsoftonline.com/logout.srf?post_logout_redirect_uri=${redirectUri}`;
 </script>
 
 <template>
