f3f784626150305bdbf600fbfaa8df077054bf98
Leon Ehart <1034@htl.rennweg.at>
2026-01-16 19:24:15 +0100

    Fix: import onMounted in LogoutView

--- frontend/src/views/LogoutView.vue
+++ frontend/src/views/LogoutView.vue
@@ -6,13 +6,16 @@ onMounted(() => {
   localStorage.removeItem("token");
   document.cookie = "auth_token=; Path=/; Max-Age=0; SameSite=None; Secure";
 
-  // Microsoft Logout + Redirect zur Login-Seite
   const redirectUri = encodeURIComponent(
     "https://transparent-grading-flax.vercel.app/login"
   );
 
   window.location.href =
-    `https://login.microsoftonline.com/common/oauth2/v2.0/logout` +
+    "https://login.microsoftonline.com/common/oauth2/v2.0/logout" +
     `?post_logout_redirect_uri=${redirectUri}`;
 });
 </script>
+
+<template>
+  <div>Abmeldung...</div>
+</template>
