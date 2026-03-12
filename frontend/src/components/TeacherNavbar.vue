b19ba3653ba099a338f5874cc26636fe92c6eebc
Leon Ehart <1034@htl.rennweg.at>
2026-03-12 08:11:17 +0100

    admin nav

--- frontend/src/components/TeacherNavbar.vue
+++ frontend/src/components/TeacherNavbar.vue
@@ -77,6 +77,21 @@
             </svg>
             Logout
         </router-link>
+        <router-link to="/admin/klassen" class="nav-item" active-class="active">
+    <svg class="svg_nav" width="24" height="24" viewBox="0 0 24 24" fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+            stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path
+            d="M8 12H16"
+            stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path
+            d="M12 8V16"
+            stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+    Admin Bereich
+</router-link>
     </nav>
   </div>
 </template>
