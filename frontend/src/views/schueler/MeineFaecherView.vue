46df4a25e3ed30aa3391778cd9dd627c39c0e5d6
alexivanov07 <1042@htl.rennweg.at>
2026-03-12 11:19:48 +0100

    a-z button bei schüleransicht entfernt

--- frontend/src/views/schueler/MeineFaecherView.vue
+++ frontend/src/views/schueler/MeineFaecherView.vue
@@ -13,7 +13,6 @@
       <button class="btn" :class="{ active: tab === 'alle' }" @click="tab = 'alle'">Alle</button>
       <button class="btn" :class="{ active: tab === 'visible' }" @click="tab = 'visible'">Eingeblendete</button>
       <button class="btn" :class="{ active: tab === 'hidden' }" @click="tab = 'hidden'">Ausgeblendete</button>
-      <button class="btn" @click="toggleSorting">Sortiert A–Z</button>
     </div>
 
     <input v-model="searchTerm" type="text" class="search-input" placeholder="Nach Fächern suchen..." />
@@ -61,7 +60,6 @@ const router = useRouter();
 const searchTerm = ref("");
 const subjects = ref([]);
 const tab = ref("alle");
-const sortByName = ref(false);
 
 const geburtsdatum = ref(null); // Neuer State für das Geburtsdatum
 const elternEmail = ref(null); // Neuer State für die Eltern-Email
@@ -208,10 +206,6 @@ async function toggleNotif(id) {
   }
 }
 
-function toggleSorting() {
-  sortByName.value = !sortByName.value;
-}
-
 const visibleSubjects = computed(() => {
   let list = subjects.value;
 
@@ -222,9 +216,6 @@ const visibleSubjects = computed(() => {
     list = list.filter(s =>
       s.fach_name.toLowerCase().includes(searchTerm.value.toLowerCase())
     );
-
-  if (sortByName.value)
-    list = stableSortSubjects(list);
 
   return list;
 });
