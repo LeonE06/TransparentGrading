d94f6bb2a30162ad33c770b36116c4cda4f621dc
Lara Ehart <1033@htl.rennweg.at>
2026-03-19 15:19:22 +0100

    schuler lehrer wächsel

--- backend/src/Service/MicrosoftUserService.php
+++ backend/src/Service/MicrosoftUserService.php
@@ -53,15 +53,16 @@ class MicrosoftUserService
         [$localPart] = explode('@', $emailLower);
 
         // Schüler-Mail (4 Ziffern) → Lehrer
-if (preg_match('/^[0-9]{4}$/', $localPart)) {
-    $this->ensureSchueler($existingUser, $vorname, $nachname);
-    return 'Schueler';
-}
 
-if (preg_match('/^[a-z]{3}$/', $localPart)) {
-    $this->ensureLehrer($existingUser, $vorname, $nachname);
-    return 'Lehrer';
-}
+        if (preg_match('/^[a-z]{3}$/', $localPart)) {
+            $this->ensureSchueler($existingUser, $vorname, $nachname);
+            return 'Schueler';
+        }
+
+        if (preg_match('/^[0-9]{4}$/', $localPart)) {
+            $this->ensureLehrer($existingUser, $vorname, $nachname);
+            return 'Lehrer';
+        }
 
         return 'Unbekannt';
     }
