1d3cffb3c09473e6fe068c5881b96459935b17ae
Leon Ehart <1034@htl.rennweg.at>
2026-05-18 14:04:33 +0200

    schuler to lehrer

--- backend/src/Service/MicrosoftUserService.php
+++ backend/src/Service/MicrosoftUserService.php
@@ -54,16 +54,15 @@ class MicrosoftUserService
 
         // Schüler-Mail (4 Ziffern) -> Schüler
         if (preg_match('/^[0-9]{4}$/', $localPart)) {
-            $this->ensureSchueler($existingUser, $vorname, $nachname);
-            return 'Schueler';
-        }
-        // Lehrer-Mail (3 Buchstaben) -> Lehrer
-        if (preg_match('/^[a-z]{3}$/', $localPart)) {
             $this->ensureLehrer($existingUser, $vorname, $nachname);
             return 'Lehrer';
         }
+        // Lehrer-Mail (3 Buchstaben) -> Lehrer
+        if (preg_match('/^[a-z]{3}$/', $localPart)) {
+            $this->ensureSchueler($existingUser, $vorname, $nachname);
+            return 'Schueler';
+        }
 
-        
         return 'Unbekannt';
     }
 
